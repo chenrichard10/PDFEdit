56b9c46ab8dfb998376641f16a60e0af28d2679c
Richard Chen <richard.chen1@uwaterloo.ca>
2020-05-04 16:57:15 -0400

    Merging PDFs now works

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
+import pdf_edit as edit
 import tkinter as tk
 from tkinter import filedialog
+import os
 # os.startfile(opens the specified )
 class Application(tk.Tk):
     def __init__(self):
@@ -10,18 +12,19 @@ class Application(tk.Tk):
         self.iconbitmap('C:/Users/chenp/Documents/GitHub/PDFEdit/res/pdf.ico')
         self.create_widgets()
 
-    """def merge(self):
-        first_pdf = "temp"
-        second_pdf = "temp"
-        combined_pdf = "temp"""
-        
-
-
-
-
+    # This function opens 3 separate file dialogs to find the combined PDF and location 
+    # to be saved
+    # Merged file is opened afterwards 
+    def merge(self):
+        pdf1 = tk.filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select first PDF")
+        pdf2 = tk.filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select second PDF")
+        location = tk.filedialog.askdirectory(initialdir = os.getcwd(), title = "Select a folder to be stored")
+        combined = edit.merge_pdfs('merged.pdf', location, pdf1, pdf2)
+        os.startfile(combined)
+    
     def create_widgets(self):
         # Creating the buttons 
-        self.merge_btn = tk.Button(self, text = "Merge PDF", padx = 50, pady = 50)
+        self.merge_btn = tk.Button(self, text = "Merge PDF", padx = 50, pady = 50, command = self.merge)
         self.convert_btn = tk.Button(self, text = "Convert to .docx", padx = 45, pady = 50)
         self.edit_btn = tk.Button(self, text = "Edit PDF", padx = 56, pady = 50)
         self.extract_btn = tk.Button(self, text = "Extra Info", padx = 64, pady = 50)
@@ -30,10 +33,6 @@ class Application(tk.Tk):
         self.convert_btn.grid(row = 2, column = 4,  padx = 10, pady = 20)
         self.edit_btn.grid(row = 4, column = 2,  padx = 10, pady = 20)
         self.extract_btn.grid(row = 4, column = 4,  padx = 10, pady = 20)
-
-    def greet(self):
-        print("Greetings!")
-
 
 if __name__ == '__main__':
     app = Application()
