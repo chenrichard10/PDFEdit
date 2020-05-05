bee14e43b25684ba91e24e817607fced2687c232
Richard Chen <richard.chen1@uwaterloo.ca>
2020-05-05 16:01:35 -0400

    Spitting page functionality

--- gui.py
+++ gui.py
@@ -1,16 +1,34 @@
 import pdf_edit as edit
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import font as tkfont
 import os
-# os.startfile(opens the specified )
+
+
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        # Main window is not resizable for now 
         self.resizable(0, 0)
-        self.geometry("400x400")
+        self.geometry("400x300")
         self.title("PDFEdit")
         self.iconbitmap('C:/Users/chenp/Documents/GitHub/PDFEdit/res/pdf.ico')
         self.create_widgets()
+
+
+    # Creating widgets for the starting screen 
+    def create_widgets(self):
+        # Creating the buttons 
+        self.merge_btn = tk.Button(self, font =("Arial", "12"), text = "Merge PDF", padx = 30, pady = 30, command = self.merge, fg = '#ffffff', bg = '#ff0000')
+        self.convert_btn = tk.Button(self,font =("Arial", "12"), text = "Convert to .docx", padx = 25, pady = 30, fg = '#ffffff',  bg = '#ff0000')
+        self.edit_btn = tk.Button(self, font =("Arial", "12"), text = "Edit PDF", padx = 36, pady = 30, fg = '#ffffff',  bg = '#ff0000', command = self.edit_pdf)
+        self.extract_btn = tk.Button(self, font =("Arial", "12"), text = "Extra Info", padx = 44, pady = 30, fg = '#ffffff', bg = '#ff0000')
+        # Positioning the buttons 
+        self.merge_btn.grid(row = 2, column = 2,  padx = 10, pady = 20)
+        self.convert_btn.grid(row = 2, column = 4,  padx = 10, pady = 20)
+        self.edit_btn.grid(row = 4, column = 2,  padx = 10, pady = 20)
+        self.extract_btn.grid(row = 4, column = 4,  padx = 10, pady = 20)
+
 
     # This function opens 3 separate file dialogs to find the combined PDF and location 
     # to be saved
@@ -21,20 +39,42 @@ class Application(tk.Tk):
         location = tk.filedialog.askdirectory(initialdir = os.getcwd(), title = "Select a folder to be stored")
         combined = edit.merge_pdfs('merged.pdf', location, pdf1, pdf2)
         os.startfile(combined)
-    
-    def create_widgets(self):
-        # Creating the buttons 
-        self.merge_btn = tk.Button(self, text = "Merge PDF", padx = 50, pady = 50, command = self.merge)
-        self.convert_btn = tk.Button(self, text = "Convert to .docx", padx = 45, pady = 50)
-        self.edit_btn = tk.Button(self, text = "Edit PDF", padx = 56, pady = 50)
-        self.extract_btn = tk.Button(self, text = "Extra Info", padx = 64, pady = 50)
-        # Positioning the buttons 
-        self.merge_btn.grid(row = 2, column = 2,  padx = 10, pady = 20)
-        self.convert_btn.grid(row = 2, column = 4,  padx = 10, pady = 20)
-        self.edit_btn.grid(row = 4, column = 2,  padx = 10, pady = 20)
-        self.extract_btn.grid(row = 4, column = 4,  padx = 10, pady = 20)
+
+
+    # Edit_pdf creates a new window to create a new_pdf from a user specified page range
+    def edit_pdf(self):
+        top = tk.Toplevel()
+        top.title("Edit your Pdf")
+        top.iconbitmap('C:/Users/chenp/Documents/GitHub/PDFEdit/res/pdf.ico')
+        top.geometry("400x400")
+        label = tk.Label(top, font =("Arial", "12"), text = "Enter the starting page",pady = 20)
+        label2 = tk.Label(top, font =("Arial", "12"), text = "Enter the ending page", pady = 20)
+        label3 = tk.Label(top, font =("Arial", "12"), text = "File name", pady = 20)
+        start = tk.Entry(top, width = 35, borderwidth = 5)
+        end = tk.Entry(top, width = 35, borderwidth = 5)
+        file = tk.Entry(top, width = 35, borderwidth = 5)
+        button = tk.Button(top, font =("Arial", "12"), text = "Create PDF", padx = 30, pady = 20, 
+                            fg = '#ffffff', bg = '#ff0000', command = lambda: 
+                            self.create_split_pdf(file.get()+".pdf", start.get(), end.get()))
+        label.pack()
+        start.pack()
+        label2.pack()
+        end.pack()
+        label3.pack()
+        file.pack()
+        button.pack()
+
+
+    # A wrapper function for create_split in pdf_edit.py
+    # Opens newly created pdf and prompts user to select pdf and folder to store
+    def create_split_pdf(self, output_name, start, end): 
+        file = tk.filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select PDF")
+        folder = tk.filedialog.askdirectory(initialdir = os.getcwd(), title = "Select Folder To Store")
+        new_file = edit.create_split(file, output_name, folder, start, end)
+        os.startfile(new_file)
+
 
 if __name__ == '__main__':
     app = Application()
     app.mainloop()
-
+    
--- pdf_edit.py
+++ pdf_edit.py
@@ -1,10 +1,12 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
-# Merging PDFs 
+
+# merge_pdfs appends pdf2 to the end of pdf1
+# returns the location of the new pdf file
 def merge_pdfs(output_name, location, pdf1, pdf2):
     paths = []
     paths.append(pdf1)
     paths.append(pdf2)
-    location = location + '/'+ output_name;
+    location = location + '/'+ output_name
     pdf_writer = PdfFileWriter()
 
     for path in paths:
@@ -17,15 +19,27 @@ def merge_pdfs(output_name, location, pdf1, pdf2):
     with open(location, 'wb') as out:
         pdf_writer.write(out)
 
-    print(location)
     return location
-        
 
+
+# create_split creates a new pdf from a given pdf 
+# with page ranging from (start, end)
+# returns location of newly created pdf
+def create_split(pdf, output_name, location, start, end):
+    start = int(start)
+    end   = int(end) + 1
+    location = location + '/'+ output_name
+    pdf_writer = PdfFileWriter()
+    pdf_reader = PdfFileReader(pdf)
+    for page in range(start, end):
+        pdf_writer.addPage(pdf_reader.getPage(page))
+    with open(location, 'wb') as out:
+        pdf_writer.write(out)
+    print(location)
+    return location 
 
     
 
-
-
-
+    
 
 
