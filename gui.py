5b5044c7631aaeb9bf62e8bb35b7d122c8627af9
Felix He <felixhe.ski@outlook.com>
2020-05-06 12:59:34 -0400

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,6 +2,11 @@ import pdf_edit as edit
 import tkinter as tk
 from tkinter import filedialog
 import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from docx import Document
+from docx.shared import Inches
+import sys
+import comtypes.client
 # os.startfile(opens the specified )
 class Application(tk.Tk):
     def __init__(self):
@@ -21,13 +26,39 @@ class Application(tk.Tk):
         location = tk.filedialog.askdirectory(initialdir = os.getcwd(), title = "Select a folder to be stored")
         combined = edit.merge_pdfs('merged.pdf', location, pdf1, pdf2)
         os.startfile(combined)
+
+    def pdfToDocx(self):
+        document = Document()
+        document.add_heading('Document Title', 0)
+        text=""
+        pdf_file = tk.filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select PDF")
+        pdf_file
+        read_pdf = PdfFileReader(pdf_file)
+        c = read_pdf.numPages
+        print('gunga')
+        for i in range(c):
+            page = read_pdf.getPage(i)
+            text+=(page.extractText())
+        
+        document.add_paragraph(text)
+        document.add_page_break()
+        document.save(read_pdf.documentInfo.title + '.docx')
+
+    def docxtoPDF(self):
+        wdFormatPDF = 17
+        word = comtypes.client.CreateObject('Word.Application')
+        doc = tk.filedialog.askopenfilename(initialdir = os.getcwd(), title = "Select first PDF")
+        doc.SaveAs(doc.Title + '.pdf', FileFormat=wdFormatPDF)
+        doc.Close()
+        word.Quit() 
+
     
     def create_widgets(self):
         # Creating the buttons 
         self.merge_btn = tk.Button(self, text = "Merge PDF", padx = 50, pady = 50, command = self.merge)
         self.convert_btn = tk.Button(self, text = "Convert to .docx", padx = 45, pady = 50)
         self.edit_btn = tk.Button(self, text = "Edit PDF", padx = 56, pady = 50)
-        self.extract_btn = tk.Button(self, text = "Extra Info", padx = 64, pady = 50)
+        self.extract_btn = tk.Button(self, text = "Convert to .pdf", padx = 64, pady = 50)
         # Positioning the buttons 
         self.merge_btn.grid(row = 2, column = 2,  padx = 10, pady = 20)
         self.convert_btn.grid(row = 2, column = 4,  padx = 10, pady = 20)
